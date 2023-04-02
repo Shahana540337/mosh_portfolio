fa083bbfcd276b20cc7179b9af92413a793428f0
Shahana Akter <you@example.com>
2023-04-02 17:49:08 +0600

    MOSH

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$('.review-part').slick({
+    dots: false,
+    arrows:false,
+    infinite: true,
+    speed:300,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0px',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  
+  $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+  });
